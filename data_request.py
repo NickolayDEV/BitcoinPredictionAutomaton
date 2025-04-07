6ff177613adeb643c64d07c53f0dea13163fbc64
NickolayDEV <nicksilantiev@gmail.com>
2025-04-07 18:14:10 +0300

    The API has been refined

--- data_request.py
+++ data_request.py
@@ -2,27 +2,27 @@
 from datetime import datetime, timedelta
 from dateutil import tz
 import requests
-API_KEY = "REMOVED"
-symbol = "BTC"
+def get_current_currency():
+    API_KEY = "REMOVED"
+    symbol = "BTC"
 
-url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
+    url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
 
-params = {
-    "symbol": "BTC",
-    "convert": "USD"
-}
+    params = {
+        "symbol": "BTC",
+        "convert": "USD"
+    }
 
-headers = {
-    "Accepts": "application/json",
-    "X-CMC_PRO_API_KEY": API_KEY
-}
+    headers = {
+        "Accepts": "application/json",
+        "X-CMC_PRO_API_KEY": API_KEY
+    }
 
-response = requests.get(url, headers=headers, params=params)
-data = response.json()
+    response = requests.get(url, headers=headers, params=params)
+    data = response.json()
 
-price = data["data"]["BTC"]["quote"]["USD"]["price"]
-market_cap = data["data"]["BTC"]["quote"]["USD"]["market_cap"]
+    price = data["data"]["BTC"]["quote"]["USD"]["price"]
+    market_cap = data["data"]["BTC"]["quote"]["USD"]["market_cap"]
 
-print(f"💰 Price now: {price}")
-print(f"🏦 Market Cap now: {market_cap}")
+    return market_cap,price
 
--- predict.py
+++ predict.py
@@ -5,29 +5,64 @@ from model import myLSTM  # Заменить на твою архитектур�
 import pandas as pd
 import asyncio
 import joblib
-# --- Загрузка модели ---
-model = myLSTM()
+import numpy as np
+from data_request import get_current_currency
+from torch.utils.data import TensorDataset,DataLoader
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(device)
+input_dim = 2
+output_dim = 1
+hidden_dim = 512
+layer_dim = 2
+batch_size = 64
+dropout = 0.3
+n_epochs = 200
+learning_rate = 5e-5
+weight_decay = 1e-4
+#Возможно придется перенести модель на CPU
+model_params = {'input_dim': input_dim,
+                'hidden_dim' : hidden_dim,
+                'layer_dim' : layer_dim,
+                'output_dim' : output_dim,
+                'dropout_prob' : dropout,
+                'device' : device}
+model = myLSTM(**model_params)
 model.load_state_dict(torch.load("model2.pth"))
+model=model.to('cuda')
 model.eval()
-scaler = joblib.load("scaler.pkl")
+scaler = joblib.load("scaler.joblib")
 
 def predict():
-    df = pd.read_csv("latest_data.csv")  # Или твой способ получения данных
-    input_tensor = torch.tensor(df.values).float().unsqueeze(0)
+    df = np.array(get_current_currency()).reshape(1,-1)
+    print(df)
+    df=scaler.transform(df)
+    print(df)
+    
+    input_tensor = torch.Tensor(df)
+    input_tensor=TensorDataset(input_tensor)
+    input_tensor=DataLoader(input_tensor, batch_size=1, shuffle=False)
+    predictions=[]
     with torch.no_grad():
-        prediction = model(input_tensor).item()
-    return prediction
+        for batch in input_tensor:
+            x=batch[0]
+            x=x.unsqueeze(1).to('cuda')
+            output=model(x)   
+            predictions.append(output.item())
+    return predictions
 
 # --- Асинхронная отправка в Telegram ---
-async def send_prediction():
+#async def send_prediction():
+def send_prediction():
+    pred = predict()
     TOKEN = "ТВОЙ_ТОКЕН"
     CHANNEL_ID = "@название_канала"  # или ID, если канал приватный
     bot = Bot(token=TOKEN)
-    pred = predict()
+    
     text = f"🔮 Прогноз курса BTC на завтра: {pred:.2f} USD"
-    await bot.send_message(CHANNEL_ID, text)
-    await bot.session.close()
-
+    #await bot.send_message(CHANNEL_ID, text)
+    #await bot.session.close()
+send_prediction()
 # --- Запуск скрипта ---
-if __name__ == "__main__":
-    asyncio.run(send_prediction())
+#if __name__ == "__main__":
+#    asyncio.run(send_prediction())
